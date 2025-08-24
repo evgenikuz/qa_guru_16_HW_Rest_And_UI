6645fd801dd12937bc65fec4dd9d10881ac58456
Evgeniia Kharitonova <kuznetsovaevgeni@gmail.com>
2025-08-24 23:23:56 +0300

    added add book method

--- src/test/java/tests/DeleteTest.java
+++ src/test/java/tests/DeleteTest.java
@@ -1,10 +1,12 @@
 package tests;
 
-import io.restassured.http.Cookies;
-import models.LoginResponseModel;
-import models.LoginBodyModel;
+import com.codeborne.selenide.Selenide;
+import models.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,9 +14,7 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.openqa.selenium.devtools.v135.network.Network.setCookie;
 import static specs.RequestSpec.requestSpec;
 import static specs.ResponseSpec.responseSpec;
 
@@ -25,7 +25,8 @@ public class DeleteTest extends TestBase {
         LoginBodyModel userData = new LoginBodyModel();
         userData.setUserName("kate_smith");
         userData.setPassword("Katesmith9$");
-        LoginResponseModel response = step("Make request", () ->
+
+        LoginResponseModel loginResponse = step("Make login request", () ->
                 given(requestSpec)
                         .body(userData)
 
@@ -36,24 +37,73 @@ public class DeleteTest extends TestBase {
                         .spec(responseSpec(200))
                         .extract().as(LoginResponseModel.class));
 
-        step("Check response", () -> {
-            assertEquals(userData.getUserName(), response.getUsername());
-            assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImthdGVfc21pdGgiLCJwYXNzd29yZCI6IkthdGVzbWl0aDkkIiwiaWF0IjoxNzU2MDMzNTYxfQ.jFb7sg2mXcTcaZjXODHXmKOZmfZAumaFXLYZao2S9Eg", response.getToken());
-            assertEquals("450c031d-1bc6-4338-92a9-c5972049648c", response.getUserId());
+        step("Check login successful", () -> {
+            assertEquals(userData.getUserName(), loginResponse.getUsername());
+            assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImthdGVfc21pdGgiLCJwYXNzd29yZCI6IkthdGVzbWl0aDkkIiwiaWF0IjoxNzU2MDMzNTYxfQ.jFb7sg2mXcTcaZjXODHXmKOZmfZAumaFXLYZao2S9Eg", loginResponse.getToken());
+            assertEquals("450c031d-1bc6-4338-92a9-c5972049648c", loginResponse.getUserId());
+        });
+
+        AddListOfBooksBodyModel bookData = new AddListOfBooksBodyModel();
+        bookData.setUserId(loginResponse.getUserId());
+        List<CollectionOfIsbnsModel> isbnList = new ArrayList<>();
+        CollectionOfIsbnsModel isbn1 = new CollectionOfIsbnsModel();
+        isbn1.setIsbn("9781449325862");
+        isbnList.add(isbn1);
+        CollectionOfIsbnsModel isbn2 = new CollectionOfIsbnsModel();
+        isbn2.setIsbn("9781449331818");
+        isbnList.add(isbn2);
+        bookData.setCollectionOfIsbns(isbnList);
+        AddListOfBooksResponseModel bookResponse = step("Make add list of books request", () ->
+                given(requestSpec)
+                        .header("Authorization", "Bearer " + loginResponse.getToken())
+                        .body(bookData)
+
+                        .when()
+                        .post("/BookStore/v1/Books")
+
+                        .then()
+                        .spec(responseSpec(201))
+                        .extract().as(AddListOfBooksResponseModel.class));
+
+        step("Check books are added", () -> {
+            assertEquals("9781449325862", bookResponse.getBooks().get(0).getIsbn());
+            assertEquals("9781449331818", bookResponse.getBooks().get(1).getIsbn());
         });
 
         step("Authorization with api", () -> {
         open("/favicon.ico");
-        getWebDriver().manage().addCookie(new Cookie("userName", response.getUsername()));
-        getWebDriver().manage().addCookie(new Cookie("userID", response.getUserId()));
-        getWebDriver().manage().addCookie(new Cookie("token", response.getToken()));
-        getWebDriver().manage().addCookie(new Cookie("expires", response.getExpires()));
+        getWebDriver().manage().addCookie(new Cookie("userName", loginResponse.getUsername()));
+        getWebDriver().manage().addCookie(new Cookie("userID", loginResponse.getUserId()));
+        getWebDriver().manage().addCookie(new Cookie("token", loginResponse.getToken()));
+        getWebDriver().manage().addCookie(new Cookie("expires", loginResponse.getExpires()));
         });
 
-        open("/profile");
-        executeJavaScript("$('footer').remove();");
-        executeJavaScript("$('#fixedban').remove();");
-        $("#userName-value").shouldHave(text(userData.getUserName()));
+        step("Open UI profile", () -> {
+            open("/profile");
+            executeJavaScript("$('footer').remove();");
+            executeJavaScript("$('#fixedban').remove();");
+            $("#userName-value").shouldHave(text(userData.getUserName()));
+        });
 
+        step("Delete a book with UI", () -> {
+            $$(".mr-2").findBy(text("Learning JavaScript Design Patterns")).closest(".rt-tr").$("#delete-record-undefined").click();
+        });
+
+        step("Confirm removal of a book with UI", () -> {
+            $("#closeSmallModal-ok").click();
+            Selenide.confirm();
+        });
+
+        AddListOfBooksResponseModel bookResponse = step("Confirm removal with api", () ->
+                given(requestSpec)
+                        .header("Authorization", "Bearer " + loginResponse.getToken())
+                        .body(bookData)
+
+                        .when()
+                        .post("/BookStore/v1/Books")
+
+                        .then()
+                        .spec(responseSpec(201))
+                        .extract().as(AddListOfBooksResponseModel.class));
     }
 }
